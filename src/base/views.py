da811ce44c69f5193b13a1b54f2c434a93135d51
Leandro Arturi <lea.arturi@gmail.com>
2022-05-20 17:47:09 -0300

    CRUD Tareas

--- src/base/views.py
+++ src/base/views.py
@@ -1,6 +1,39 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+
+from .models import Tarea
+
+class ListaPendientes(ListView):
+    model = Tarea
+    context_object_name = 'tareas'
+    template_name = 'base/tareas.html'
+
+    
+class DetalleTarea(DetailView):
+    model = Tarea
+    context_object_name = 'tarea'
+    template_name = 'base/tarea.html'
 
 
-def lista_pendientes(request):
-    return HttpResponse("Lista de Pendientes")
+class CrearTarea(CreateView):
+    model = Tarea
+    fields = ['title', 'description']
+    template_name = 'base/tarea_form.html'
+    success_url = reverse_lazy('tareas')
+    
+    
+class EditarTarea(UpdateView):
+    model = Tarea
+    fields = ['title', 'description', 'completed']
+    template_name = 'base/tarea_form.html'
+    success_url = reverse_lazy('tareas')
+    
+    
+class EliminarTarea(DeleteView):
+    model = Tarea
+    context_object_name = 'tarea'
+    template_name = 'base/tarea_delete.html'
+    success_url = reverse_lazy('tareas')
